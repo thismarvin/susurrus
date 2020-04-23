6ebf027202fe11d68de8ac54ecceab4625b81fdd
thismarvin <bend_@outlook.com>
2020-04-23 18:57:09 -0500

    improved constructor + create color from hex code

--- src/color.mjs
+++ src/color.mjs
@@ -1,8 +1,20 @@
 export default class Color {
     constructor(r, g, b, a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a ? a : 255;
+        this.r = r || 0;
+        this.g = g || 0;
+        this.b = b || 0;
+        this.a = a || 255;
+    }
+
+    toArray() {
+        return [this.r, this.g, this.b, this.a];
+    }
+
+    static fromHex(hex, a) {
+        const r = (hex & 0xFF0000) >> 16;
+        const g = (hex & 0xFF00) >> 8;
+        const b = hex & 0xFF;
+
+        return new Color(r, g, b, a || 255);
     }
 }
