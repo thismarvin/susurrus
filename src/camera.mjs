9b7928e24754fd315ef1a069cb930e1bcd90a808
thismarvin <bend_@outlook.com>
2020-04-23 18:55:33 -0500

    removed temporary offset

--- src/camera.mjs
+++ src/camera.mjs
@@ -3,10 +3,10 @@ import Vector3 from "./vector3.mjs";
 
 export default class Camera {
     constructor() {
-        this.world = Matrix.createTranslation(1, 0, 0);
+        this.world = Matrix.createTranslation(0, 0, 0);
         this.view = Matrix.createLookAt(new Vector3(0, 0, 1), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
-        this.projection = Matrix.createOrthographic(4, 4, 0, 16);                
-        
+        this.projection = Matrix.createOrthographic(4, 4, 0, 16);
+
         this.worldViewProjection = Matrix.multiply(Matrix.multiply(this.view, this.world), this.projection);;
     }
 }
