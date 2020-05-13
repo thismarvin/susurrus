5a4e63639c0422042d6ba86ee0dd5839d778ddde
thismarvin <bend_@outlook.com>
2020-05-13 00:06:17 -0500

    Camera reflects new Matrix changes

--- src/utilities/camera.ts
+++ src/utilities/camera.ts
@@ -1,23 +1,34 @@
-import Matrix from "../maths/matrix.js";
+import MatrixTransform from "../maths/matrixTransform.js";
 import Vector3 from "../maths/vector3.js";
+import Matrix from "../maths/matrix.js";
 
 export default class Camera {
-	public world: Matrix;
-	public view: Matrix;
-	public projection: Matrix;
-	public worldViewProjection: Matrix;
+	public world: MatrixTransform;
+	public view: MatrixTransform;
+	public projection: MatrixTransform;
+	public worldViewProjection: MatrixTransform;
 
-	constructor() {
-		this.world = Matrix.createTranslation(0, 0, 0);
-		this.view = Matrix.createLookAt(
+	constructor(width: number, height: number) {
+		this.world = MatrixTransform.createTranslation(0, 0, 0);
+		this.view = MatrixTransform.createLookAt(
 			new Vector3(0, 0, 1),
 			new Vector3(0, 0, 0),
 			new Vector3(0, 1, 0)
 		);
-		this.projection = Matrix.createOrthographic(4, 4, 0, 16);
+		this.projection = MatrixTransform.createOrthographic(width, height, 0, 16);
 
-		this.worldViewProjection = this.view
-			.multiply(this.world)
-			.multiply(this.projection);
+		this.worldViewProjection = Matrix.mult(
+			Matrix.mult(this.view, this.world),
+			this.projection
+		);
+	}
+
+	public setBounds(width: number, height: number) {
+		this.projection = MatrixTransform.createOrthographic(width, height, 0, 16);
+
+		this.worldViewProjection = Matrix.mult(
+			Matrix.mult(this.view, this.world),
+			this.projection
+		);
 	}
 }
