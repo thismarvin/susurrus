001c0c6d612a1c12ac2fa806c7573ed1d2919ad1
thismarvin <bend_@outlook.com>
2020-05-14 23:22:40 -0500

    added a Rectangle class

---
+++ src/maths/rectangle.ts
@@ -0,0 +1,48 @@
+export default class Rectangle {
+	public readonly x: number;
+	public readonly y: number;
+	public readonly width: number;
+	public readonly height: number;
+
+	public get top() {
+		return this.y;
+	}
+	public get right() {
+		return this.x + this.width;
+	}
+	public get bottom() {
+		return this.y + this.height;
+	}
+	public get left() {
+		return this.x;
+	}
+
+	constructor(x: number, y: number, width: number, height: number) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		Object.defineProperty(this, "x", {
+			writable: false,
+		});
+		Object.defineProperty(this, "y", {
+			writable: false,
+		});
+		Object.defineProperty(this, "width", {
+			writable: false,
+		});
+		Object.defineProperty(this, "height", {
+			writable: false,
+		});
+	}
+
+	public intersects(rectangle: Rectangle) {
+		return (
+			this.left < rectangle.right &&
+			this.right > rectangle.left &&
+			this.top < rectangle.bottom &&
+			this.bottom > rectangle.top
+		);
+	}
+}
