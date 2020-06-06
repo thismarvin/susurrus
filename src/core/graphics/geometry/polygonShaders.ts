17f96e40224a9a02ab01fdef718c5f54dce8ccfb
thismarvin <bend_@outlook.com>
2020-06-06 17:06:38 -0500

    fixed weird formatting

--- src/core/graphics/geometry/polygonShaders.ts
+++ src/core/graphics/geometry/polygonShaders.ts
@@ -39,7 +39,7 @@ const POLYGON_SHADERS = {
         }
 
         void main() {
-						mat4 transform = createTranslation(a_translation - a_rotationOffset) * createRotationZ(a_rotation) * createTranslation(a_rotationOffset) * createScale(a_scale);
+			mat4 transform = createTranslation(a_translation - a_rotationOffset) * createRotationZ(a_rotation) * createTranslation(a_rotationOffset) * createScale(a_scale);
             gl_Position =  worldViewProjection * transform * vec4(a_position, 1);
             v_color = a_color;
         }
