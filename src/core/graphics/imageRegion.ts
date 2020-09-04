600f2d285ffe99ef8e6c1d2dda45821c82d9be60
thismarvin <bend_@outlook.com>
2020-09-04 17:28:09 -0500

    image region is no longer readonly

--- src/core/graphics/imageRegion.ts
+++ src/core/graphics/imageRegion.ts
@@ -1,26 +1,13 @@
 export default class ImageRegion {
-	public readonly x: number;
-	public readonly y: number;
-	public readonly width: number;
-	public readonly height: number;
+	public x: number;
+	public y: number;
+	public width: number;
+	public height: number;
 
 	constructor(x: number, y: number, width: number, height: number) {
 		this.x = Math.floor(x);
 		this.y = Math.floor(y);
 		this.width = Math.floor(width);
 		this.height = Math.floor(height);
-
-		Object.defineProperty(this, "x", {
-			writable: false,
-		});
-		Object.defineProperty(this, "y", {
-			writable: false,
-		});
-		Object.defineProperty(this, "width", {
-			writable: false,
-		});
-		Object.defineProperty(this, "height", {
-			writable: false,
-		});
 	}
 }
