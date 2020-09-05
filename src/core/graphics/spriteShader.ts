b73ae31b55e4365abf6a8737acb334ae1b302c74
thismarvin <bend_@outlook.com>
2020-09-05 17:56:55 -0500

    updated sprite shader

--- src/core/graphics/spriteShader.ts
+++ src/core/graphics/spriteShader.ts
@@ -3,34 +3,63 @@ const SPRITE_SHADERS = {
 		uniform mat4 worldViewProjection;
 
 		attribute vec3 a_vertexPosition;
-		attribute vec2 a_textureCoord;
 		attribute vec3 a_scale;
+		attribute vec3 a_translation;
+		attribute vec3 a_origin;
+		attribute vec3 a_rotation;
+		attribute vec4 a_color;
+		attribute vec2 a_textureCoord;
 
+		varying lowp vec4 v_color;
 		varying highp vec2 v_textureCoord;
 
-		mat4 createScale(vec3 scale) {
+		mat4 createTranslation(vec3 translation) {
 			return mat4(
-					scale.x, 0, 0, 0,
-					0, scale.y, 0, 0,
-					0, 0, scale.z, 0,
-					0, 0, 0, 1
+				1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				translation.x, translation.y, translation.z, 1
 			);
 		}
 
-		void main() {
-			mat4 model = createScale(a_scale);
-			gl_Position = worldViewProjection * model * vec4(a_vertexPosition, 1);
+		mat4 createScale(vec3 scale) {
+			return mat4(
+				scale.x, 0, 0, 0,
+				0, scale.y, 0, 0,
+				0, 0, scale.z, 0,
+				0, 0, 0, 1
+			);
+		}
 
+		mat4 createRotation(vec3 rotation) {
+			return mat4(
+				cos(rotation.z) * cos(rotation.y), sin(rotation.z) * cos(rotation.y), -sin(rotation.y), 0,
+				cos(rotation.z) * sin(rotation.y) * sin(rotation.x) - sin(rotation.z) * cos(rotation.x), sin(rotation.z) * sin(rotation.y) * sin(rotation.x) + cos(rotation.z) * cos(rotation.x), cos(rotation.y) * sin(rotation.x), 0,
+				cos(rotation.z) * sin(rotation.y) * cos(rotation.x) + sin(rotation.z) * sin(rotation.x), sin(rotation.z) * sin(rotation.y) * cos(rotation.x) - cos(rotation.z) * sin(rotation.x), cos(rotation.y) * cos(rotation.x), 0,
+				0, 0, 0, 1
+			);
+		}
+
+		mat4 caclulateTransform(vec3 scale, vec3 translation, vec3 origin, vec3 rotation) {
+			return createTranslation(translation + origin) * createRotation(rotation) * createTranslation(-origin) * createScale(scale);
+		}
+
+		void main() {
+			mat4 transform = caclulateTransform(a_scale, a_translation, a_origin, a_rotation);
+			gl_Position = worldViewProjection * transform * vec4(a_vertexPosition, 1);
+
+			v_color = a_color;
 			v_textureCoord = a_textureCoord;
 		}
 		`,
 	FRAGMENT: `
 		uniform sampler2D sampler;
 
+		varying lowp vec4 v_color;
 		varying highp vec2 v_textureCoord;
 
 		void main() {
-			gl_FragColor = texture2D(sampler, v_textureCoord);
+			gl_FragColor = texture2D(sampler, v_textureCoord) * v_color;
 		}
     `,
 };
